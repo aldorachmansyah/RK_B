88d8d4a39a5897106c64a7f7d11e2266efbad02b
aldorachmansyah <aldorachmansyah@gmail.com>
2017-05-22 16:21:52 +0700

    jadwal service

--- Detail_Jadwal_Service.php
+++ Detail_Jadwal_Service.php
@@ -40,13 +40,8 @@
 	</nav>
 		
 	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
-		<form role="search">
-			<div class="form-group">
-				<input type="text" class="form-control" placeholder="Search">
-			</div>
-		</form>
-		<ul class="nav menu"> 
-			<li><a href="Detail_Jadwal_Service.php"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg>Jadwal Service</a></li>
+		<ul class="nav menu">
+			<li class="active"><a href="Jadwal_Service.php"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-table"></use></svg> Jadwal Service</a></li>
 			<li role="presentation" class="divider"></li>
 
 		</ul>
@@ -56,8 +51,9 @@
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
 		<div class="row">
 			<ol class="breadcrumb">
-				<li><a href="home.php"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li><a href="Jadwal_Service.php"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
 				<li class="active">Jadwal Service</li>
+				<li class="active">Detail</li>
 			</ol>
 		</div><!--/.row-->
 		
@@ -66,10 +62,80 @@
 				<h1 class="page-header">Dashboard</h1>
 			</div>
 		</div>-->
-		<div style="text-align: center;">
-			<h1>Selamat Datang</h1>
-			<h2>Sistem Informasi Peminjaman Mobil</h2>
-			<h1>ForRent</h1>
+		<h1></h1>
+
+		<table border="1" style="border-collapse: collapse;width: 100%;">
+			<thead>
+				<tr style="background: #C9C9C9;">
+					<th style="text-align: center;padding: 8px">Nopol</th>
+					<th style="text-align: center;">Merk</th>
+					<th style="text-align: center;">Jenis</th>
+					<th style="text-align: center;">Muatan (orang)</th>
+					<th style="text-align: center;">Driver</th>
+					<th style="text-align: center;">Harga Sewa (per hari)</th>
+					<th style="text-align: center;width: 130px">#</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr style="text-align: center;">
+					<td>B 510 NDA</td>
+					<td>Kia</td>
+					<td>Rio</td>
+					<td>5</td>
+					<td>Ario Bimo</td>
+					<td>250000</td>
+					<td style="padding: 3px;"><a style="color: black" href="ubah_data_armada.php"><button>Ubah</button></a> <a style="color: black" href="#"><button>Hapus</button></a></td>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr style="text-align: center;">
+					<td>B 4707 BAN</td>
+					<td>Honda</td>
+					<td>City</td>
+					<td>4</td>
+					<td>Bimo Ario</td>
+					<td>250000</td>
+					<td style="padding: 3px;"><a style="color: black" href="ubah_data_armada.php"><button>Ubah</button></a> <a style="color: black" href="#"><button>Hapus</button></a></td>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr style="text-align: center;">
+					<td>L 1712 KL</td>
+					<td>Suzuki</td>
+					<td>Ertiga</td>
+					<td>6</td>
+					<td>Sumila</td>
+					<td>350000</td>
+					<td style="padding: 3px;"><a style="color: black" href="ubah_data_armada.php"><button>Ubah</button></a> <a style="color: black" href="#"><button>Hapus</button></a></td>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr style="text-align: center;">
+					<td>D 123 VE</td>
+					<td>Toyota</td>
+					<td>Innova</td>
+					<td>7</td>
+					<td>Faizul</td>
+					<td>400000</td>
+					<td style="padding: 3px;"><a style="color: black" href="ubah_data_armada.php"><button>Ubah</button></a> <a style="color: black" href="#"><button>Hapus</button></a></td>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr style="text-align: center;">
+					<td>W 120 SI</td>
+					<td>Toyota</td>
+					<td>Agya</td>
+					<td>4</td>
+					<td>Sina</td>
+					<td>280000</td>
+					<td style="padding: 3px;"><a style="color: black" href="ubah_data_armada.php"><button>Ubah</button></a> <a style="color: black" href="#"><button>Hapus</button></a></td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div align="center">
+			<h1></h1>
+			<a href="tambah_data_armada.php" style="color: black"><button type="button">Tambah Data</button></a>
 		</div>
 
 	</div>	<!--/.main-->
