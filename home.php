2e9aefd49138b2667f57f7f4fdd0c5e6ced88601
pramudito <ditosmileupp@gmail.com>
2017-05-23 03:21:27 +0700

    edit positioning

--- home.php
+++ home.php
@@ -7,46 +7,50 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
-	<div class="navbar">
-		<a href="index.html" class="logo font1"><b class="font2">ForRent</a>
-	</div>
+<body> 
+	<div class="container-fluid">
+		<div class="navbar">
+			<a href="index.html" class="logo font1"><b class="font2">ForRent</a>
+		</div>
+	</div><!-- /.container-fluid -->
+
+
 	<div class="col-md-12 badan">
 		<div class="col-md-2"></div>
 		<div class="col-md-8">
-			<div class="col-md-12 center">
+			<div class="col-md-12 center" style="margin-top: 216px">
 				<a href="Kendaraan_Driver.php"><div class="col-md-4 daftar ">
-					<div class="putih">
+					<div class="putih" style="height: 100px">
 						<img src="">
 						<p class="font2">Daftar Kendaraan & Driver</p>
 					</div>
 				</div></a>
 				<a href="Status_Kendaraan.php"><div class="col-md-4 daftar ">
-					<div class="putih">
+					<div class="putih" style="height: 100px">
 						<img src="">
 						<p class="font2">Status Kendaraan</p>
 					</div>
 				</div></a>
 				<a href="Jadwal_Service.php"><div class="col-md-4 daftar ">
-					<div class="putih">
+					<div class="putih" style="height: 100px">
 						<img src="">
 						<p class="font2">Jadwal Service Kendaraan</p>
 					</div>
 				</div></a>
 				<a href="jadwal_rekap_kendaraan.php"><div class="col-md-4 daftar ">
-					<div class="putih">
+					<div class="putih" style="height: 100px">
 						<img src="">
 						<p class="font2">Jadwal Rekap Kendaraan</p>
 					</div>
 				</div></a>
 				<a href="notifikasi.php"><div class="col-md-4 daftar ">
-					<div class="putih">
+					<div class="putih" style="height: 100px">
 						<img src="">
 						<p class="font2">Notifikasi</p>
 					</div>
 				</div></a>
 				<a href="penyewaan.php"><div class="col-md-4 daftar ">
-					<div class="putih">
+					<div class="putih" style="height: 100px">
 						<img src="">
 						<p class="font2">Penyewaan</p>
 					</div>
