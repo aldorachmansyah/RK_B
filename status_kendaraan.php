7e9fa4a2ae1f1ce6fe6a3819fa2901b3a24f5a7f
AHMAD-ISMAIL\Ahmad Ismail <x5ahmadismail@gmail.com>
2017-05-29 22:05:41 +0700

    no message

--- status_kendaraan.php
+++ status_kendaraan.php
@@ -331,14 +331,7 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.3.8
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-    reserved.
-  </footer>f
-  <div class="control-sidebar-bg"></div>
+
 </div>
 <!-- ./wrapper -->
 
