208ea55f7c5c2652bbd3ae8a815b365a35734e7d
aldorachmansyah <aldorachmansyah@gmail.com>
2017-05-30 08:18:38 +0700

    perubahan title pada index.php, tambahan data service bagian driver (tidak terhubung)

--- Versi 3/index.php
+++ Versi 3/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Forms</title>
+<title>Login</title>
 
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/datepicker3.css" rel="stylesheet">
